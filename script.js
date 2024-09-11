3abd4d624f933cd543a1407c3c49326ef0e88e58
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-09-11 16:18:00 +0600

    Add hover effect so grid divs change color

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ for (let i = 0; i < 16; i++) {
   rowElement.classList = "row";
   for (let j = 0; j < 16; j++) {
     const itemElement = document.createElement("div");
-    // itemElement.textContent = `${i*16 + j}`
     itemElement.classList = "item";
     rowElement.appendChild(itemElement);
   }
   containerElement.appendChild(rowElement);
 }
+
+function randomColorSelector() {
+    const red = Math.floor(Math.random()*256);
+    const green = Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+    return `rgb(${red},${green},${blue})`;
+}
+
+containerElement.addEventListener("mouseover", (e) => {
+    e.target.classList.contains("item") && (e.target.style.background = randomColorSelector());
+});
+containerElement.addEventListener("mouseout", (e) => {
+    e.target.classList.contains("item") && (e.target.style.background = "");
+});
